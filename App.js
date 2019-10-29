7aacd550aa7b4d7a77dabaa64bf6815373a4db7e
kingjaejun <saqxtmxhf888@naver.com>
2019-10-29 15:28:57 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Map from './screens/Map';
+
+export default function App() {
+  return (
+    <Map />
+  );
+}
+
+
